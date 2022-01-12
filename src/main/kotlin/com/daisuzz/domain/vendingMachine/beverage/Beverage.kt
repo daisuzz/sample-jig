29a3869002e39329b255ca368e01fcc5afab4f81
daisuzz <daisaku.rugby@gmail.com>
2022-01-12 22:57:13 +0900

    add kdoc to each Beverage classes

--- src/main/kotlin/com/daisuzz/domain/vendingMachine/beverage/Beverage.kt
+++ src/main/kotlin/com/daisuzz/domain/vendingMachine/beverage/Beverage.kt
@@ -4,8 +4,20 @@ package com.daisuzz.domain.vendingMachine.beverage
  * 飲み物
  */
 sealed class Beverage(val id: BeverageId, private val price: Int) {
+
+    /**
+     * 水
+     */
     class Water : Beverage(BeverageId(1), 100)
+
+    /**
+     * お茶
+     */
     class Tea : Beverage(BeverageId(2), 150)
+
+    /**
+     * コーヒー
+     */
     class Coffee : Beverage(BeverageId(3), 200)
 
     fun canBuy(money: Int): Boolean {
